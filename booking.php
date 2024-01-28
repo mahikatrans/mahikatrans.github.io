f1cfd59678b7574334aa225cc236d88d859f3a02
mahikatrans <157908764+mahikatrans@users.noreply.github.com>
2024-01-28 15:31:00 +0700

    Create booking.php

---
+++ booking.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3pro.css">
+<style>
+* {
+  box-sizing: border-box;
+}
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+}
+
+label {
+  padding: 12px 12px 12px 0;
+  display: inline-block;
+}
+
+input[type=submit] {
+  background-color: #0f4d92;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  float: right;
+}
+
+input[type=submit]:hover {
+  background-color: #000;
+}
+
+.container {
+  border-radius: 5px;
+  background-color: #4169E1;
+  padding: 20px;
+  color: white;
+}
+
+.col-25 {
+  float: left;
+  width: 25%;
+  margin-top: 6px;
+}
+
+.col-75 {
+  float: left;
+  width: 75%;
+  margin-top: 6px;
+}
+
+/* Clear floats after the columns */
+.row::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .col-25, .col-75, input[type=submit] {
+    width: 100%;
+    margin-top: 0;
+  }
+}
+</style>
+</head>
+<body>
+<form action="booking.php" method="post">
+ <div style="overflow-x:auto;">
+<fieldset>
+
+<!-- Form Name -->
+<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiQ4eTxeoa1nSHbbhFjw33oRgjlCdB1tW6z8VdFU9e_nt8ihNWQ46QHZu-4sZBWevaoyCzfjO42nY9Q2o6LKtzaMPAaaN7iK0jO41uq6FFOoVLIFxAW0cWi-xl8ynPE7SsoUzk5ZUfLGJVQkqEUmqpWLeo3V60SSnNx58KoiLxyD3GdvOQxIo56uugwnw80/s2065/logo-atas.png" width="120" height="48">
+
+<!-- Text input-->
+<div class="form-group">
+<div class="container">
+  <div class="row">
+    <div class="col-25">
+      <label for="nama">Nama</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="name" name="nama" >
+    </div>
+  </div>
+
+<div class="row">
+    <div class="col-25">
+      <label for="hp">No Hp</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="hp" name="hp" >
+    </div>
+  </div>
+
+
+  <div class="row">
+    <div class="col-25">
+      <label for="tujuan">Tujuan</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="tujuan" name="tujuan" >
+    </div>
+  </div>
+
+
+  <div class="row">
+    <div class="col-25">
+      <label for="armada">Armada</label>
+    </div>
+    <div class="col-75">
+      <select id="armada" name="armada">
+        <option value="Murni Jaya">Murni Jaya</option>
+        <option value="Agra Mas">Agra Mas</option>
+        <option value="Brave Trans">Brave Trans</option>
+        <option value="Cahaya Trans">Cahaya Trans</option>
+      </select>
+    </div>
+  </div>
+
+<div class="row">
+    <div class="col-25">
+      <label for="seat">No Kursi</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="seat" name="seat" >
+    </div>
+  </div>
+
+ <div class="row">
+    <div class="col-25">
+      <label for="seat">Harga</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="harga" name="harga" >
+    </div>
+  </div>
+
+   <div class="row">
+  <div class="col-25">
+      <label for="berangkat">Pemberangkatan:</label>
+    </div>
+    <div class="col-75">
+      <input type="date" id="tanggal" name="tanggal" >
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <input type="submit" value="BOOKING" name="simpan">
+  </div>
+  </form>
+</div>
+</div>
+</body>
+</html>
+
+
